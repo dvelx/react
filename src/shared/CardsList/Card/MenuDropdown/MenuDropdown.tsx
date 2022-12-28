c6d67a2221c5fa2e317104eae8648a001183ca6c
prostoG <49344670+prostoG@users.noreply.github.com>
2022-12-28 16:38:33 +0300

    help me

--- src/shared/CardsList/Card/MenuDropdown/MenuDropdown.tsx
+++ src/shared/CardsList/Card/MenuDropdown/MenuDropdown.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, {MouseEventHandler, useEffect, useRef} from 'react';
 import styles from './menudropdown.css';
 import {Dropdown} from "../../../Dropdown";
 import {MenuIcon} from "../../../Icons";
 import {MenuDropdownList} from "../../../MenuDropdownList";
 
 interface IMenuDropdown {
-    id?: string
+    id?: string;
+    onClick?: () => void
 }
 
-export function MenuDropdown({id}: IMenuDropdown) {
+export function MenuDropdown({id, onClick}: IMenuDropdown) {
+
+    const ref = useRef<HTMLButtonElement>(null)
+
+    useEffect(() => {
+        const onClick = (e: MouseEvent) =>e.target instanceof Node && ref.current?.contains(e.target) || console.log(e.target)
+        document.addEventListener('click', onClick)
+        return () => document.removeEventListener('click', onClick)
+    }, [])
 
   return (
     <div>
       <Dropdown
         button={
-        <button className={styles.menuButton} id={id}>
+        <button className={styles.menuButton} id={id} ref={ref}>
           <MenuIcon />
         </button>
       }>
